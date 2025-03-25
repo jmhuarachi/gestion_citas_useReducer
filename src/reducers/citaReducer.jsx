4ec24b047e4dbbc0a7e293ccd6c600cf3424da56
jmHuarachi <manuelhuarachi003@email.com>
2025-03-25 11:21:42 -0400

    Archivos de Gestion de Citas con useReducer

---
+++ src/reducers/citaReducer.jsx
@@ -0,0 +1,49 @@
+import {citas_db} from '../data/citas_db.js';
+
+const localStorageCitas = () => {
+    const citas = localStorage.getItem('listaDeCitas')
+    return citas ? JSON.parse(citas) : citas_db
+}
+
+//Estado inicial
+export const initialState = {
+    listaDeCitas: localStorageCitas(),
+    seleccionarCita: ''
+}
+//Reducer
+export const citaReducer = (state, action) => {
+    if (action.type === 'SELECT_CITA') {
+        const seleccionarCita = state.listaDeCitas.find(cita => cita.id === action.id)
+        return {
+            ...state,
+            seleccionarCita
+        }
+    }
+    if(action.type === 'ACTUALIZAR_CITA'){
+        const listaDeCitas = state.listaDeCitas.map(cita => cita.id === action.id ? {...cita, available: action.available} : cita)
+        return {
+            ...state,
+            listaDeCitas
+        }
+    }
+    if(action.type === 'RESERVAR_CITA'){
+        const listaDeCitas = state.listaDeCitas.map(cita => cita.id === action.id ? {...cita, available: false} : cita)
+        return {
+            ...state,
+            listaDeCitas
+        }
+    }
+    if(action.type === 'CANCELAR_CITA'){
+        const listaDeCitas = state.listaDeCitas.map(cita => cita.id === action.id ? {...cita, available: true} : cita)
+        return {
+            ...state,
+            listaDeCitas
+        }
+    }
+    if(action.type === 'RESET_FORM'){
+        return {
+            listaDeCitas: citas_db,
+            seleccionarCita: ''
+        }
+    }
+}
